8ac5ed04796f1e714a663358627712494512a97a
AkashYadav-02 <akashydv2212@gmail.com>
2021-08-20 20:08:59 +0530

    intial commit

---
+++ travello/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Destination
+
+
+# Create your views here.
+
+
+def index(request):
+    dests = Destination.objects.all()
+
+    return render(request, "index.html", {'dests': dests})
+
+
+def contact(request):
+    dest = Destination.objects.all()
+
+    return render(request, 'contact.html', {'dests': dest})
